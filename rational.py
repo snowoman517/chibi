93b1f85bc89b0a561618d91b0fedaff2a2b28bbb
SHIBATA Azusa <21716046@jwu.ac.jp>
2019-10-07 16:26:52 +0900

    5/6

--- rational.py
+++ rational.py
@@ -3,7 +3,16 @@ class Q(object):
         self.a = a
         self.b = b
     def __repr__(self):
+        if self.b == 1:
+            return str(self.a)
         return f'{self.a}/{self.b}'
+    def add(self, q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d+b*c, b*d)
 
-q = Q(1,2)
-print(q)
+q1 = Q(1,2)
+q2 = Q(1,3)
+print(q1.add(q2))
