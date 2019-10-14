244c9e38979f62a7051e23c66ae0db09717f39c5
SHIBATA Azusa <21716046@jwu.ac.jp>
2019-10-14 15:59:19 +0900

    Add

--- exp.py
+++ exp.py
@@ -13,3 +13,23 @@ v = Val(1)
 print(v)
 assert v.eval() == 1
 
+class Add(object):
+    __slots__=['left', 'right']
+    def __init__(self, a, b):
+        self.left = a
+        self.right= b
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+
+e = Add(Val(1), Val(2))
+print(e.eval())
+assert v.eval() == 3
+        
+e = Add(Val(1),Add(Val(2),Val(3)))
+print(e.eval())
+
+
+print()
+print()
+
