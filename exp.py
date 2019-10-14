5e360f1fe2beb82b3c77dcb8c952d481a7401796
SHIBATA Azusa <21716046@jwu.ac.jp>
2019-10-14 16:52:38 +0900

    Binary

--- exp.py
+++ exp.py
@@ -1,6 +1,8 @@
-assert 1+1==2
+class Expr(object):
+    pass
 
-class Val(object):
+
+class Val(Expr):
     __slots__ = ['value']
     def __init__(self, value = 0):
         self.value = value
@@ -13,23 +15,41 @@ v = Val(1)
 print(v)
 assert v.eval() == 1
 
-class Add(object):
+assert isinstance(v, Expr)
+assert isinstance(v, Val)
+assert not isinstance(v, int)
+
+def toExpr(a):
+    if not isinstance(a, Expr):
+            a = Val(a)
+    return a
+
+class Binary(Expr):
+    def __repr__(self):
+        classname = self.__class__.__name__
+        return f'{classname}({self.left},{self.right})'
+
+
+class Add(Expr):
     __slots__=['left', 'right']
     def __init__(self, a, b):
+        if not isinstance(a, Expr):
+            a = Val(a)
+        if not isinstance(b, Expr):
+            b = Val(b)
         self.left = a
         self.right= b
     def eval(self):
         return self.left.eval() + self.right.eval()
 
 
-e = Add(Val(1), Val(2))
-assert v.eval() == 3
+e = Add(1, Add(1,2))
+print(e.eval())
+assert e.eval() == 4
         
+e = Add(1,2)
+assert e.eval() == 3        
+
 e = Add(Val(1),Add(Val(2),Val(3)))
 assert v.eval() == 6
-
-
-
-print()
-print()
 
