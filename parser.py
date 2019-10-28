a613a3f4e17336bb4e4f07e8aff7a20b11c770b0
SHIBATA Azusa <21716046@jwu.ac.jp>
2019-10-28 16:50:39 +0900

    output Expr

--- parser.py
+++ parser.py
@@ -1,30 +1,32 @@
-from exp import Val, Add
+from exp import Val, Add, Mul, Sub, Div
 
 def parse(s: str):
-    num = int(s)
-    return Val(num)
-'''
-e = parse("123")
-print(e)
-
-s = "123+456"
-pos = s.find('+')
-print('pos', pos)
-
-s1 = s[0:pos]
-s2 = s[pos+1:]
-print(s, s1, s2)
-'''
-
-def parse(s: str):
-    pos = s.find('+')
-    if pos == -1:
-        num = int(s)
-        return Val(num)
-    else:
+    if s.find('+') > 0:
+        pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1),parse(s2))
 
-e = parse("1+2+3")
+    if s.rfind('-') > 0:
+        pos = s.find('-')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Sub(parse(s1),parse(s2))
+
+    if s.find('*') > 0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1),parse(s2))
+
+
+    if s.find('/') > 0:
+        pos = s.find('/')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Div(parse(s1),parse(s2))
+    return Val(int(s))
+    
+e = parse("1-2-3")
 print(e, e.eval())
+assert e.eval()
