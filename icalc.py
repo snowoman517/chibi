6fe587811b38e9ec7988be645d5992be2dc02836
SHIBATA Azusa <21716046@jwu.ac.jp>
2019-11-25 16:50:53 +0900

    icalc2

--- icalc.py
+++ icalc.py
@@ -7,22 +7,76 @@ Value = { [0-9]+ #Int }
 ''')
 parser = pegpy.generate(peg)
 
-t = parser('1+2*3')
-print(repr(t))
 
-t = parser('@2')
-print(repr(t))
+class Expr(object):
+    @classmethod
+    def new(cls, v):
+        if isinstance(v,Expr):
+            return v
+        return Val(v)
 
-def calc(t):
-    if t == 'Int':
-         return int(str(t))
-    if t == 'Add':
-         return calc(t[0]) + calc(t[1])
-    if t == 'Mul':
-         return calc(t[0]) * calc(t[1])
-    print(f'TODO {t.tag}')
-    return 0
+class Val(Expr):
+    __slot__=['value']
 
-t = parser('1+2*3+4*5')
-print(repr(t))
-print(calc(t))
+    def__init__(self, value):
+        self.value = value
+
+    def__repr__(self):
+        return f'Val({self.value})'
+
+    def eval(self, env: dict):
+        retrun self.value
+
+e = Val(0)
+assert e.eval({}) == 0
+
+class Binary(Expr):
+    __slot__ = ['left', 'right']
+
+    def __init__(self,left,right):
+        self.left = Expr.new(left)
+        self.right = Expr.new(right)
+
+    def __repr__(self):
+        classname = self.__class__.__name__
+        return f'{classname}({self.left},{self.right})'
+    
+class Add(Binary):
+    __slot__ = ['left', 'right']
+
+    def eval(self, env: dict):
+        retrun self.left.eval(env) + self.right.eval(env)
+
+def conv(tree):
+    if tree == 'Block':
+        return conv(tree[0])
+    if tree == 'Val' or tree == 'Int':
+        return Val(int(str(tree)))
+    if tree == 'Add':
+        return Add(conv(tree[0]), conv(tree[1]))
+    print('@TODO', tree.tag)
+    return Val(str(tree))
+
+def run(stc: str):
+    tree = parser(stc)
+    if tree.isError():
+        print(repr(tree))
+    else:
+        e = conv(tree)
+        print(repr(e))
+        print(e.eval({}))
+
+
+
+def main():
+    try:
+        while True: 
+             s = input('>>>')
+             if s == '':
+                 break
+            run(s)
+    expect EOFError:
+         return
+
+if __name__ == '__main__':
+    main()
